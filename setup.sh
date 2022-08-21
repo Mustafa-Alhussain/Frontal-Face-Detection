720cfcfce71dab8a7daf5604710b9fa2ba44e996
Mustafa-Alhussain <mustafa.alhussain97@gmail.com>
2022-08-21 17:03:28 +0300

    15th commit

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "mustafa.alhussain97@gmail.com"n
+
+echo "\
+[general]\n\
+email = \"mustafa.alhussain97@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
