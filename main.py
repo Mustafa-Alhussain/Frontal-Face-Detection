5e3e065f382ae116620d52980a807d0b05f7bd70
Mustafa-Alhussain <mustafa.alhussain97@gmail.com>
2022-08-23 14:36:29 +0300

    37th

--- main.py
+++ main.py
@@ -31,8 +31,10 @@ path = os.path.join(path1 ,"models")
 st.cache(allow_output_mutation=True)
 
 # -------------Test1------------------------------------------------
-
-filename1 = os.path.join(path ,"model_gen.sav")
+/app/frontal-face-detection/
+#filename1 = os.path.join(path ,"model_gen.sav")
+filename1 = "/app/frontal-face-detection/models/model_gen.sav"
+#filename1 = "models/model_gen.sav"
 gender_loaded_model = pickle.load(open(filename1, 'rb'))
 filename2 = os.path.join(path ,"emotion_model.sav")
 emotion_loaded_model = pickle.load(open(filename2, 'rb'))
